1aeaf60895c1ab17b841b3ced51312af85f161b7
Gabriela <g.martinez.rodz@gmail.com>
2021-12-21 20:30:35 +0000

    Testing fixes in base_model

--- models/base_model.py
+++ models/base_model.py
@@ -46,6 +46,7 @@ class BaseModel:
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[1]).split('\'')[0]
+        print(self.__class__.__name__)
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):

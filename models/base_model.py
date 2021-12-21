53dc13239fc10f4b2c8b53b437c69ffe1d5e229e
Sergio Vera <sergio.vera@icloud.com>
2021-12-21 19:44:07 +0000

    console debugged

--- models/base_model.py
+++ models/base_model.py
@@ -8,30 +8,40 @@ from models import storage
 
 Base = declarative_base()
 
+
 class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-
         id = Column(String(60), primary_key=True, nullable=False)
-        #created_at = Column(datetime, nullable=False, default=datetime.utcnow())
-        #updated_at = Column(datetime, nullable=False, default=datetime.utcnow())
+#       created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+#       updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-        if '__class__' in kwargs:
-            del kwargs['__class__']
-           # self.__dict__.update(kwargs)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            else:
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                         '%Y-%m-%dT%H:%M:%S'
+                                                         '.%f')
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
+            else:
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                         '%Y-%m-%dT%H:%M:%S'
+                                                         '.%f')
+            if '__class__' in kwargs:
+                del kwargs['__class__']
+#            self.__dict__.update(kwargs)
 
-        for key, val in kwargs.items():
-            setattr(self, key, val)
+            for key, val in kwargs.items():
+                setattr(self, key, val)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -53,6 +63,8 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if '_sa_instance_state' in dictionary.keys():
+            del dictionary['_sa_instance_state']
         return dictionary
 
     def delete(self):

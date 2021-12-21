719f181b8badc0f8aec75a1c9830c52059dd9c9e
Sergio Vera <sergio.vera@icloud.com>
2021-12-21 16:39:19 +0000

    creating dictionary to return class and save() method added on delete()

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,12 +12,11 @@ class FileStorage:
         """Returns a dictionary of models currently in storage"""
         if cls is None:
             return FileStorage.__objects
-        else:
-            dict_cls = {}
-            for k, v in FileStorage.__objects.items():
-                if v.__class__ == cls:
-                    dict_cls[k] = [v]
-            return FileStorage.__objects
+        dict_cls = {}
+        for k, v in self.__objects.items():
+            if v.__class__ == cls:
+                dict_cls[k] = [v]
+            return dict_cls
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -59,9 +58,9 @@ class FileStorage:
     def delete(self, obj=None):
         """ Method to delete objects """
         from models.base_model import BaseModel
-        obj = FileStorage.__objects
         if obj is None:
             pass
         else:
-            del obj
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del self.__objects[key]
             self.save()

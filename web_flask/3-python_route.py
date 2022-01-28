cf5558be9723ee2fd619d73ba0bc564e9b1756ee
Sergio Vera <sergio.vera@icloud.com>
2022-01-28 21:46:33 +0000

    task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Flask application
+Routes:
+    * /: display "Hello HBNB!"
+    * /hbnb: display "HBNB"
+    * /c/<text>: display “C ” followed by the value of the
+        text variable (replace underscore _ symbols with a space)
+"""
+
+from flask import Flask, escape
+
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    diplays HBNB
+    """
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """
+    diplays C
+    """
+    return 'C %s' % escape(text)
+
+@app.route('/python')
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text='is_cool'):
+    """
+    diplays Python route
+    """
+    return 'Python %s' % escape(text.replace('_',' '))
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=5000)

25ceb78233ff140a42d0c96b92215e42ccb9e15e
Sergio Vera <sergio.vera@icloud.com>
2022-01-29 17:12:10 +0000

    readme

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,6 +11,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
@@ -18,5 +19,6 @@ def hbnb():
     """
     return 'HBNB'
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4002)

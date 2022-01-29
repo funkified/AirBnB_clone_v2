2741d706525d5a675570aee8fa8e739327c1b52d
Sergio Vera <sergio.vera@icloud.com>
2022-01-29 06:21:42 +0000

    chaning port

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,15 +2,14 @@
 """
 script that starts a Flask web application
 """
-
 from flask import Flask
-
-
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_route():
+    """ prints message"""
     return 'Hello HBNB!'
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

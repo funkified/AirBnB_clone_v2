1692cd6e9197db03cdf5cfb211b8d401e21a753c
Sergio Vera <sergio.vera@icloud.com>
2022-01-29 16:51:36 +0000

    pep8

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -11,5 +11,7 @@ def hello_route():
     """ prints message"""
     return 'Hello HBNB!'
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    """running app"""
+    app.run(host='0.0.0.0', port='5000')

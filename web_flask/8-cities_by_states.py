5d56151753dea7809f64e2785e50e98c02f8b828
Sergio Vera <sergio.vera@icloud.com>
2022-01-30 21:40:43 +0000

    task 8

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Runs Flash Application
+"""
+from models import storage
+from models.state import State
+from models.city import City
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_data(self):
+    """ remove current SQLAlchemy Session """
+    return storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def index():
+    """displays html plage"""
+    data = storage.all(City)
+    return render_template('8-cities_by_states.html', data=data)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

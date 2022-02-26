3fc5c3fe68f854942bee97acff5a76c3547a43cf
Sergio Vera <sergio.vera@icloud.com>
2022-02-26 07:14:33 +0000

    commit from server

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,13 +3,13 @@
 script that starts a Flask web application
 """
 from flask import Flask
-app = Flask(__name__)
+app = Flask('app')
 
 
-@app.route('/', strict_slashes=False)
-def hello_route():
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello_hbnb():
     """ prints message"""
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
 
 if __name__ == '__main__':
